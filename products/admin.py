4d09259ed6f1e1bcddea026345ca8a3d29b48b4f
gemaquejr <gemaquejr@hotmail.com>
2024-09-03 22:10:54 -0300

    Registrando os models em admin.py
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Supermarket, Price
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Supermarket)
+admin.site.register(Price)
