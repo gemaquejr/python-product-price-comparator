5db638be65cbaf548bf7d77775bc0f6d2bcd3dd2
gemaquejr <gemaquejr@hotmail.com>
2024-09-03 22:02:08 -0300

    Definindo Models
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- products/models.py
+++ products/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Supermarket(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Price(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    supermarket = models.ForeignKey(Supermarket, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    date_checked = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.product} - {self.supermarket} - {self.price}"
