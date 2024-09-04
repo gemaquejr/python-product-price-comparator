a0bd791747bee5c0617732bb0a9a66b8c6a4bcc8
gemaquejr <gemaquejr@hotmail.com>
2024-09-03 22:16:37 -0300

    Criando views básicas em views.py
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product, Price
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
+
+
+def price_comparison(request, product_id):
+    product = Product.objects.get(id=product_id)
+    prices = Price.objects.filter(product=product)
+    return render(request, 'products/price_comparison.html', {'product': product, 'prices': prices})
