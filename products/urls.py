82cb96f10983751c4ddd1ad2440288ed6b2be10e
gemaquejr <gemaquejr@hotmail.com>
2024-09-04 01:02:36 -0300

    Adiciona funcionalidades de incluir produtos manualmente
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('product/<int:product_id>/compare/', views.price_comparison, name='price_comparison'),
+    path('add_product/', views.add_product, name='add_product'),
+    path('add_price/', views.add_price, name='add_price'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,13 +1,40 @@
-from django.shortcuts import render
-from .models import Product, Price
+from django.shortcuts import render, redirect
+from .models import Product, Price, Supermarket
 
 
 def product_list(request):
     products = Product.objects.all()
-    return render(request, 'products/product_list.html', {'products': products})
+    supermarkets = Supermarket.objects.all()
+    return render(request, 'products/product_list.html', {'products': products, 'supermarkets': supermarkets})
 
 
 def price_comparison(request, product_id):
     product = Product.objects.get(id=product_id)
     prices = Price.objects.filter(product=product)
     return render(request, 'products/price_comparison.html', {'product': product, 'prices': prices})
+
+
+def add_product(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        category = request.POST.get('category')
+        if name and category:
+            Product.objects.create(name=name, description=description, category=category)
+        return redirect('product_list')
+    else:
+        return render(request, 'products/product_list.html')
+
+
+def add_price(request):
+    if request.method == 'POST':
+        product_id = request.POST.get('product')
+        supermarket_id = request.POST.get('supermarket')
+        price = request.POST.get('price')
+        if product_id and supermarket_id and price:
+            product = Product.objects.get(id=product_id)
+            supermarket = Supermarket.objects.get(id=supermarket_id)
+            Price.objects.create(product=product, supermarket=supermarket, price=price)
+        return redirect('product_list')
+    else:
+        return redirect('product_list')
